8865f08303eb464dd8cb9675c0bb60b95e593874
Carlos Flores <carlos@floresbenavides.com>
2023-06-09 13:36:37 -0600

    move template files to template directory

--- src/builder.mjs
+++ src/builder.mjs
@@ -15,29 +15,32 @@ import { resolve } from 'path'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const publicDir = resolve(__dirname, '../public/')
-const partials = resolve(__dirname, 'partials/')
-const siteParams = yaml.load(rf(resolve(__dirname, '../site.yml')))
+const templateDir = resolve(__dirname, 'template/')
+const defaultParams = yaml.load(rf(resolve(templateDir, 'site.yml')))
+const newParams = yaml.load(rf(resolve(__dirname, '../site.yml')))
 
-const markups = ['index']
-const styles = ['styles']
+const definitions = Object.assign(defaultParams, newParams)
+const { markups, styles } = definitions.params
+const siteParams = JSON.parse(JSON.stringify(definitions))
+delete siteParams.params
 
 rm(publicDir, { recursive: true, force: true })
 mk(resolve(publicDir))
 mk(resolve(publicDir, 'images'))
 
 console.log('- Migrating images'.magenta)
-const images = rd(resolve(__dirname, 'images'))
+const images = rd(resolve(templateDir, 'images'))
 images.forEach(image => {
-  cf(resolve(__dirname, 'images', image), resolve(publicDir, 'images', image))
+  cf(resolve(templateDir, 'images', image), resolve(publicDir, 'images', image))
 })
-const favicon = rd(resolve(__dirname, 'favicon'))
+const favicon = rd(resolve(templateDir, 'favicon'))
 favicon.forEach(item => {
-  cf(resolve(__dirname, 'favicon', item), resolve(publicDir, item))
+  cf(resolve(templateDir, 'favicon', item), resolve(publicDir, item))
 })
 
 console.log('- Building Markup files'.magenta)
 markups.forEach(item => {
-  const template = pug.renderFile(resolve(__dirname, `${item}.pug`), {
+  const template = pug.renderFile(resolve(templateDir, `${item}.pug`), {
     pretty: true,
     ...siteParams
   })
@@ -45,8 +48,9 @@ markups.forEach(item => {
 })
 
 console.log('- Building Style files'.magenta)
+const partials = resolve(templateDir, 'partials/')
 styles.forEach(async item => {
-  const source = resolve(__dirname, `${item}.styl`)
+  const source = resolve(templateDir, `${item}.styl`)
   const css = await new Promise((resolve, reject) => {
     const string = rf(source, 'utf8')
     stylus(string)
