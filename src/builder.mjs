5dba2071e05a04c2a123f73e80f3a645173b049b
Carlos Flores <carlos@floresbenavides.com>
2023-06-09 18:24:17 -0600

    add dynamic support

--- src/builder.mjs
+++ src/builder.mjs
@@ -9,9 +9,10 @@ import {
 } from 'fs'
 import pug from 'pug'
 import stylus from 'stylus'
+import { marked } from 'marked'
 import yaml from 'js-yaml'
 import url from 'url'
-import { resolve } from 'path'
+import { resolve, extname, basename } from 'path'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const publicDir = resolve(__dirname, '../public/')
@@ -38,6 +39,89 @@ const favicon = rd(resolve(templateDir, 'favicon'))
 favicon.forEach(item => {
   cf(resolve(templateDir, 'favicon', item), resolve(publicDir, item))
 })
+
+console.log('- Loading dynamic data'.magenta)
+const getDate = input => {
+  if (!input?.meta) return {}
+  const date = input.meta.date.toString()
+  const year = date.slice(0, 4)
+  const month = date.slice(4, 6)
+  const day = date.slice(6, 8)
+  return {
+    ...input.meta,
+    date: new Date(year, month - 1, day)
+  }
+}
+const getDynamic = dir => {
+  try {
+    return rd(dir)
+      .filter(item => extname(item) === '.md')
+      .map(item => resolve(dir, item))
+      .map(file => ({ markdown: rf(file, 'utf8'), file }))
+      .map(({ markdown, file }) => {
+        const [meta, ...mkd] = markdown.split('\n\n')
+        const parsedMeta = getDate(yaml.load(meta))
+        return { markdown: mkd.join('\n\n'), file, meta: parsedMeta }
+      })
+      .map(({ markdown, file, meta }) => ({
+        html: marked.parse(markdown, { mangle: false, headerIds: false }),
+        path: basename(file, '.md'),
+        meta
+      }))
+  } catch (e) {
+    return []
+  }
+}
+
+const pages = getDynamic(resolve(__dirname, 'content'))
+const posts = getDynamic(resolve(__dirname, 'content', 'posts'))
+  .sort((a, b) => {
+    if (a.meta.date > b.meta.date) return -1
+    if (a.meta.date < b.meta.date) return 1
+    return 0
+  })
+if (posts.length > 0) mk(resolve(publicDir, 'posts'))
+
+pages
+  .map(({ html, path }) => {
+    const list = pug.renderFile(resolve(templateDir, 'partials/blogList.pug'), {
+      pretty: true,
+      posts
+    })
+    return {
+      html: html.replace('<p>!{blogList}</p>', list),
+      path
+    }
+  })
+  .forEach(({ html, path }) => {
+    const template = pug.renderFile(resolve(templateDir, 'page.pug'), {
+      pretty: true,
+      ...Object.assign(siteParams, { pageData: html })
+    })
+    mk(resolve(publicDir, path))
+    wf(resolve(publicDir, `${path}/index.html`), template)
+  })
+
+posts
+  .map((item, index) => {
+    const prev = posts[index - 1] || {}
+    const next = posts[index + 1] || {}
+    return {
+      ...item,
+      prev: { ...prev.meta, path: prev.path },
+      next: { ...next.meta, path: next.path }
+    }
+  })
+  .forEach(({ html, path, prev, next }) => {
+    const template = pug.renderFile(resolve(templateDir, 'post.pug'), {
+      pretty: true,
+      ...Object.assign(siteParams, { pageData: html }),
+      prev,
+      next
+    })
+    mk(resolve(publicDir, 'posts', path))
+    wf(resolve(publicDir, `posts/${path}/index.html`), template)
+  })
 
 console.log('- Building Markup files'.magenta)
 markups.forEach(item => {
